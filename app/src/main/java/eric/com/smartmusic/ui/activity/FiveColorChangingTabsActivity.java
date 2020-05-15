bd821bf18d57becfe11bb68f1df3b8004ff954fd
zhangchunliang <Passw0rd>
2020-05-15 17:11:16 +0800

    add test style files

--- app/src/main/java/eric/com/smartmusic/ui/activity/FiveColorChangingTabsActivity.java
+++ app/src/main/java/eric/com/smartmusic/ui/activity/FiveColorChangingTabsActivity.java
@@ -1,16 +1,14 @@
 package eric.com.smartmusic.ui.activity;
 
 import android.os.Bundle;
-import android.support.annotation.IdRes;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
-
+import eric.com.smartmusic.R;
 /**
  * Created by iiro on 7.6.2016.
  */
@@ -18,7 +16,7 @@ public class FiveColorChangingTabsActivity extends AppCompatActivity {
     private TextView messageView;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_changing_tabs);
 
@@ -27,15 +25,13 @@ public class FiveColorChangingTabsActivity extends AppCompatActivity {
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public void onTabSelected(@IdRes int tabId) {
-                messageView.setText(TabMessage.get(tabId, false));
+            public void onTabSelected(int tabId) {
             }
         });
 
         bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
             @Override
-            public void onTabReSelected(@IdRes int tabId) {
-                Toast.makeText(getApplicationContext(), TabMessage.get(tabId, true), Toast.LENGTH_LONG).show();
+            public void onTabReSelected(int tabId) {
             }
         });
     }
